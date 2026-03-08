642e2f7d9a9b10d93ca8642dfde951435ee1beff
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-08 20:28:26 +0700

    feat: add MainMenuController for main menu button action handling

---
+++ src/com/lnw/acceeeeeppt/scene/menu/MainMenuController.java
@@ -0,0 +1,26 @@
+package com.lnw.acceeeeeppt.scene.menu;
+
+import java.awt.event.ActionEvent;
+
+public class MainMenuController {
+    public void onNewGame(ActionEvent e) {
+        System.out.println("New Game");
+    }
+
+    public void onLoadGame(ActionEvent e) {
+        System.out.println("Load");
+    }
+
+    public void onOption(ActionEvent e) {
+        System.out.println("Option");
+    }
+
+    public void onCredit(ActionEvent e) {
+        System.out.println("Credit");
+    }
+
+    public void onExit(ActionEvent e) {
+        System.out.println("Exit");
+        System.exit(0);
+    }
+}
