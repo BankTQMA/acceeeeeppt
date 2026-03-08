85f16ca616cd7f0db35e409ad06e869e271473e3
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-08 17:21:23 +0700

    feat: update main method in Main class to initialize MainMenu on run

--- src/com/lnw/acceeeeeppt/main/Main.java
+++ src/com/lnw/acceeeeeppt/main/Main.java
@@ -1,7 +1,9 @@
 package com.lnw.acceeeeeppt.main;
 
+import com.lnw.acceeeeeppt.scene.menu.MainMenu;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello OOP!");
+        new MainMenu();
     }
 }
