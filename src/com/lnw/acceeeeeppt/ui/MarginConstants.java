026ce9730b4d7fa1d366248a1c114a010fbe2637
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-26 15:52:20 +0700

    feat(ui): add MarginConstants class defining a constant for margin

---
+++ src/com/lnw/acceeeeeppt/ui/MarginConstants.java
@@ -0,0 +1,9 @@
+package com.lnw.acceeeeeppt.ui;
+
+public class MarginConstants {
+    private MarginConstants() {
+        /* This utility class should not be instantiated */
+    }
+
+    public static final int GLOBALMARGIN = 15;
+}
