163b76b978e9db32cdaece075a8d3831bc7a5745
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-26 20:44:14 +0700

    feat(ui): add H2PLAINFONT font preset

--- src/com/lnw/acceeeeeppt/ui/FontPresets.java
+++ src/com/lnw/acceeeeeppt/ui/FontPresets.java
@@ -11,4 +11,5 @@ public class FontPresets {
 
     public static final Font H1BOLDFONT = new Font(MAINFONTNAME, Font.BOLD, 24);
     public static final Font H2BOLDFONT = new Font(MAINFONTNAME, Font.BOLD, 20);
+    public static final Font H2PLAINFONT = new Font(MAINFONTNAME, Font.PLAIN, 20);
 }
