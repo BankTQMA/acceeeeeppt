ea0ca08f103119c164527c0806140b6f80d02c39
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-26 11:01:43 +0700

    feat(ui): add base frame config for NewGameView

--- src/com/lnw/acceeeeeppt/scene/menu/NewGameView.java
+++ src/com/lnw/acceeeeeppt/scene/menu/NewGameView.java
@@ -1,9 +1,22 @@
 package com.lnw.acceeeeeppt.scene.menu;
 
+import java.awt.BorderLayout;
+
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
 public class NewGameView extends JPanel {
-    public NewGameView() {
+    private JPanel topBar;
+    private JPanel contentPanel;
 
+    public NewGameView() {
+        topBar = new JPanel();
+        contentPanel = new JPanel();
+
+        setLayout(new BorderLayout());
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+
+        add(topBar, BorderLayout.NORTH);
+        add(contentPanel, BorderLayout.CENTER);
     }
 }
