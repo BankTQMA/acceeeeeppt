133b10b34acec6edf436fc6689717b3b4f59c3bc
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-26 11:08:21 +0700

    feat(ui): add FontPresets for defining a font configuration globally

---
+++ src/com/lnw/acceeeeeppt/ui/FontPresets.java
@@ -0,0 +1,13 @@
+package com.lnw.acceeeeeppt.ui;
+
+import java.awt.*;
+
+public class FontPresets {
+    private FontPresets() {
+        /* This utility class should not be instantiated */
+    }
+
+    private static final String MAINFONTNAME = "Segoe UI";
+
+    public static final Font H1BOLDFONT = new Font(MAINFONTNAME, Font.BOLD, 24);
+}
