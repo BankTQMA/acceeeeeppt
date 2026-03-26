202c333273c0d9aff1d0afd4fbc436d7f1e70682
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-26 15:53:48 +0700

    feat(ui): add back button in view

--- src/com/lnw/acceeeeeppt/scene/menu/NewGameView.java
+++ src/com/lnw/acceeeeeppt/scene/menu/NewGameView.java
@@ -1,20 +1,41 @@
 package com.lnw.acceeeeeppt.scene.menu;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Cursor;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+
+import com.lnw.acceeeeeppt.ui.FontPresets;
+import com.lnw.acceeeeeppt.ui.MarginConstants;
 
 public class NewGameView extends JPanel {
     private JPanel topBar;
     private JPanel contentPanel;
+    private JButton backButton;
 
     public NewGameView() {
         topBar = new JPanel();
         contentPanel = new JPanel();
 
+        backButton = new JButton("< Back");
+        backButton.setBorderPainted(false);
+        backButton.setContentAreaFilled(false);
+        backButton.setFocusPainted(false);
+        backButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        backButton.setFont(FontPresets.H2BOLDFONT);
+        backButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+
         setLayout(new BorderLayout());
+        topBar.setLayout(new BorderLayout());
+        topBar.setBorder(BorderFactory.createEmptyBorder(MarginConstants.GLOBALMARGIN, MarginConstants.GLOBALMARGIN,
+                MarginConstants.GLOBALMARGIN, MarginConstants.GLOBALMARGIN));
         contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+
+        topBar.add(backButton, BorderLayout.WEST);
 
         add(topBar, BorderLayout.NORTH);
         add(contentPanel, BorderLayout.CENTER);
