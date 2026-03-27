5c634e16c7961d9b74839bb7c2ae250d60170693
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-27 22:02:28 +0700

    feat: register credit view to main panel

--- src/com/lnw/acceeeeeppt/main/Main.java
+++ src/com/lnw/acceeeeeppt/main/Main.java
@@ -3,6 +3,7 @@ package com.lnw.acceeeeeppt.main;
 import javax.swing.SwingUtilities;
 
 import com.lnw.acceeeeeppt.scene.MainView;
+import com.lnw.acceeeeeppt.scene.menu.CreditView;
 import com.lnw.acceeeeeppt.scene.menu.MainMenuController;
 import com.lnw.acceeeeeppt.scene.menu.MainMenuView;
 import com.lnw.acceeeeeppt.scene.menu.NewGameController;
@@ -15,12 +16,14 @@ public class Main {
             MainView mainView = new MainView();
             MainMenuView mainMenuView = new MainMenuView();
             NewGameView newGameView = new NewGameView();
+            CreditView creditView = new CreditView();
 
             new MainMenuController(mainMenuView, mainView);
             new NewGameController(mainView, newGameView);
 
             mainView.registerPanel(mainMenuView, SceneConstants.MAINMENU);
             mainView.registerPanel(newGameView, SceneConstants.NEWGAMEMENU);
+            mainView.registerPanel(creditView, SceneConstants.CREDITMENU);
 
             mainView.switchPanelCard(SceneConstants.MAINMENU);
             mainView.setFrameVisibility(true);
