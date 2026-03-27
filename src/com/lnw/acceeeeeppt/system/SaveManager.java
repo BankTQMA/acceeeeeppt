3b9471ae6bf18ef76a76ee7758b51d64fd74c5cf
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-27 15:57:04 +0700

    feat: add static method to convert byte hash to hex string

--- src/com/lnw/acceeeeeppt/system/SaveManager.java
+++ src/com/lnw/acceeeeeppt/system/SaveManager.java
@@ -7,6 +7,18 @@ public class SaveManager {
         /* This utility class should not be instantiated */
     }
 
+    public static String toHexString(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
     public static PlayerModel createNewPlayerModel(String saveName) {
         return new PlayerModel(saveName);
     }
