fc138ff8e64618fd7593c00ba4bb4e1fd08cdfc8
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-27 08:52:31 +0700

    feat(ui): add difficulty selection button handler to swap enabled state between buttons

--- src/com/lnw/acceeeeeppt/scene/menu/NewGameController.java
+++ src/com/lnw/acceeeeeppt/scene/menu/NewGameController.java
@@ -11,9 +11,21 @@ public class NewGameController {
         this.newGameView = view;
         this.mainView = mainView;
         this.newGameView.addBackButtonActionListener(e -> onBackButton());
+        this.newGameView.addStandardButtonActionListener(e -> onStandardDifficulty());
+        this.newGameView.addHardcoreButtonActionListener(e -> onHardcoreDifficulty());
     }
 
     public void onBackButton() {
         mainView.switchPanelCard(SceneConstants.MAINMENU);
+    }
+
+    public void onStandardDifficulty() {
+        newGameView.setStandardButtonEnabled(false);
+        newGameView.setHardcoreButtonEnabled(true);
+    }
+
+    public void onHardcoreDifficulty() {
+        newGameView.setHardcoreButtonEnabled(false);
+        newGameView.setStandardButtonEnabled(true);
     }
 }
