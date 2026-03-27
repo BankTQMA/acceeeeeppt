83f7bb859f5d7c074375032ab5a3bbe772cc8a86
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-27 16:03:07 +0700

    feat: add method to save player model file to disk

--- src/com/lnw/acceeeeeppt/system/SaveManager.java
+++ src/com/lnw/acceeeeeppt/system/SaveManager.java
@@ -1,5 +1,16 @@
 package com.lnw.acceeeeeppt.system;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import com.lnw.acceeeeeppt.model.PlayerModel;
 
 public class SaveManager {
@@ -21,5 +32,29 @@ public class SaveManager {
 
     public static PlayerModel createNewPlayerModel(String saveName) {
         return new PlayerModel(saveName);
+    }
+
+    public static void saveToDisk(PlayerModel playerModel) {
+        String fileName = playerModel.getSaveName();
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedHash = digest.digest(fileName.getBytes(StandardCharsets.UTF_8));
+            fileName = toHexString(encodedHash) + ".dat";
+        } catch (NoSuchAlgorithmException _) {
+            fileName += ".dat";
+        }
+        Path path = Paths.get("saves", fileName);
+
+        try {
+            Files.createDirectories(path.getParent());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path.toFile()))) {
+            oos.writeObject(playerModel);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
