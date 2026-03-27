fad760c187c6c27c84b362082777385e83853365
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-27 22:06:48 +0700

    feat(ui): add back button and add sub-container to jpanel in credit view

--- src/com/lnw/acceeeeeppt/scene/menu/CreditView.java
+++ src/com/lnw/acceeeeeppt/scene/menu/CreditView.java
@@ -1,16 +1,21 @@
 package com.lnw.acceeeeeppt.scene.menu;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Cursor;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import com.lnw.acceeeeeppt.ui.FontPresets;
 import com.lnw.acceeeeeppt.ui.MarginConstants;
 
 public class CreditView extends JPanel {
     private JPanel topBar;
     private JPanel contentPanel;
+    private JButton backButton;
 
     public CreditView() {
         // Container initialisation
@@ -23,5 +28,18 @@ public class CreditView extends JPanel {
         setBorder(BorderFactory.createEmptyBorder(MarginConstants.GLOBALMARGIN, MarginConstants.GLOBALMARGIN,
                 MarginConstants.GLOBALMARGIN, MarginConstants.GLOBALMARGIN));
         contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+
+        backButton = new JButton("< Back");
+        backButton.setBorderPainted(false);
+        backButton.setContentAreaFilled(false);
+        backButton.setFocusPainted(false);
+        backButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        backButton.setFont(FontPresets.H2BOLDFONT);
+        backButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        topBar.add(backButton, BorderLayout.WEST);
+
+        add(topBar, BorderLayout.NORTH);
+        add(contentPanel, BorderLayout.CENTER);
     }
 }
