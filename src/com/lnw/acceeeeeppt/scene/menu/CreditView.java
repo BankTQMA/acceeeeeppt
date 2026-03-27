f6697258ba512d5f9993e118e3f1d34ff1bef25b
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-27 21:59:59 +0700

    feat: add container configuration

--- src/com/lnw/acceeeeeppt/scene/menu/CreditView.java
+++ src/com/lnw/acceeeeeppt/scene/menu/CreditView.java
@@ -1,9 +1,27 @@
 package com.lnw.acceeeeeppt.scene.menu;
 
+import java.awt.BorderLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
-public class CreditView extends JPanel {
-    public CreditView() {
+import com.lnw.acceeeeeppt.ui.MarginConstants;
 
+public class CreditView extends JPanel {
+    private JPanel topBar;
+    private JPanel contentPanel;
+
+    public CreditView() {
+        // Container initialisation
+        topBar = new JPanel();
+        contentPanel = new JPanel();
+
+        // Container configuration
+        setLayout(new BorderLayout());
+        topBar.setLayout(new BorderLayout());
+        setBorder(BorderFactory.createEmptyBorder(MarginConstants.GLOBALMARGIN, MarginConstants.GLOBALMARGIN,
+                MarginConstants.GLOBALMARGIN, MarginConstants.GLOBALMARGIN));
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
     }
 }
