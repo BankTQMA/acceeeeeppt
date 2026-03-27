9d6285cda67e4885b89df13164710d162f308ada
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-27 10:48:41 +0700

    feat(system): add static method to create a new player model

--- src/com/lnw/acceeeeeppt/system/SaveManager.java
+++ src/com/lnw/acceeeeeppt/system/SaveManager.java
@@ -1,7 +1,13 @@
 package com.lnw.acceeeeeppt.system;
 
+import com.lnw.acceeeeeppt.model.PlayerModel;
+
 public class SaveManager {
     private SaveManager() {
         /* This utility class should not be instantiated */
+    }
+
+    public static PlayerModel createNewPlayerModel(String saveName) {
+        return new PlayerModel(saveName);
     }
 }
