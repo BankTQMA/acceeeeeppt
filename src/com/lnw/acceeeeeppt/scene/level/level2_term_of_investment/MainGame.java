67e33d23853f9d7cce883ac8a2caab3b607d06ec
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-27 15:12:52 +0700

    refactor: use lambda expression instead

--- src/com/lnw/acceeeeeppt/scene/level/level2_term_of_investment/MainGame.java
+++ src/com/lnw/acceeeeeppt/scene/level/level2_term_of_investment/MainGame.java
@@ -3,34 +3,34 @@ package com.lnw.acceeeeeppt.scene.level.level2_term_of_investment;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 
 // This is for testing only. Once the Main Menu is finished. Youn can delete this
 public class MainGame {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                JFrame frame = new JFrame("Term of Investment");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setSize(800, 500);
-                frame.setLocationRelativeTo(null);
+        SwingUtilities.invokeLater(() -> {
 
-                InvestModel model = new InvestModel(15);
-                InvestView view = new InvestView();
+            JFrame frame = new JFrame("Term of Investment");
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setSize(800, 500);
+            frame.setLocationRelativeTo(null);
 
-                InvestIntegration integration = new InvestIntegration() {
-                    public void navigateToMenu() {
-                        JOptionPane.showMessageDialog(frame, "Back to Menu");
-                    }
+            InvestModel model = new InvestModel(15);
+            InvestView view = new InvestView();
 
-                    public void unlockNextStage() {
-                        JOptionPane.showMessageDialog(frame, "Next Stage");
-                    }
-                };
-                InvestController controller = new InvestController(model, view, integration);
+            InvestIntegration integration = new InvestIntegration() {
+                public void navigateToMenu() {
+                    JOptionPane.showMessageDialog(frame, "Back to Menu");
+                }
 
-                frame.add(view);
-                frame.setVisible(true);
-            }
+                public void unlockNextStage() {
+                    JOptionPane.showMessageDialog(frame, "Next Stage");
+                }
+            };
+            new InvestController(model, view, integration);
+
+            frame.add(view);
+            frame.setVisible(true);
         });
     }
 }
