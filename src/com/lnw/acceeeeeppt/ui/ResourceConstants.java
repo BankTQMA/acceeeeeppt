7bf06fb89e2e9bc85a9a0f3ea124e14891fad4c2
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-27 22:30:04 +0700

    feat(ui): add ResourceConstants to store a path for resources

---
+++ src/com/lnw/acceeeeeppt/ui/ResourceConstants.java
@@ -0,0 +1,9 @@
+package com.lnw.acceeeeeppt.ui;
+
+public class ResourceConstants {
+    private ResourceConstants() {
+        /* This utility class should not be instantiated */
+    }
+
+    public static final String TOKIDOKI_ACTIONGAME_PATH = "resources/images/tokidoki_action_game.jpg";
+}
