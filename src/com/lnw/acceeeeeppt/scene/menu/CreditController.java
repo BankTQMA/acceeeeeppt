d5eef26b7d428544d223472814b9d8d8dcfdd1a4
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-28 09:12:42 +0700

    feat: open agreeee wikipedia on agreeee button action

--- src/com/lnw/acceeeeeppt/scene/menu/CreditController.java
+++ src/com/lnw/acceeeeeppt/scene/menu/CreditController.java
@@ -1,5 +1,12 @@
 package com.lnw.acceeeeeppt.scene.menu;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.swing.JOptionPane;
+
 import com.lnw.acceeeeeppt.scene.MainView;
 import com.lnw.acceeeeeppt.ui.SceneConstants;
 
@@ -20,6 +27,33 @@ public class CreditController {
     }
 
     private void onAgreeeeButton() {
+        String url = "https://ja.wikipedia.org/wiki/%E5%88%A9%E7%94%A8%E8%A6%8F%E7%B4%84%E3%81%AB%E5%90%8C%E6%84%8F%E3%81%97%E3%81%9F%E3%81%84";
+        try {
+            URI uri = new URI(url);
 
+            if (!Desktop.isDesktopSupported()) {
+                JOptionPane.showMessageDialog(creditView,
+                        "Desktop is not supported on this system.",
+                        "Desktop is not supported", JOptionPane.ERROR_MESSAGE);
+            }
+
+            Desktop desktop = Desktop.getDesktop();
+
+            if (!desktop.isSupported(Desktop.Action.BROWSE)) {
+                JOptionPane.showMessageDialog(creditView,
+                        "Browsing is not supported on this system.",
+                        "Browsing is not supported", JOptionPane.ERROR_MESSAGE);
+            }
+
+            desktop.browse(uri);
+        } catch (URISyntaxException ex) {
+            JOptionPane.showMessageDialog(creditView, "Invalid URL: " + url, "Invalid URL",
+                    JOptionPane.WARNING_MESSAGE);
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(creditView, "Failed to open browser: " + ex.getMessage(),
+                    "Failed to open browser", JOptionPane.ERROR_MESSAGE);
+            ex.printStackTrace();
+        }
     }
 }
