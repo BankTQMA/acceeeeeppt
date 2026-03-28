c61024e1b88a9a46cf79a9236932d0a6cf704cf6
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-28 13:11:33 +0700

    feat(ui): add LOADGAMEMENU scene constants

--- src/com/lnw/acceeeeeppt/ui/SceneConstants.java
+++ src/com/lnw/acceeeeeppt/ui/SceneConstants.java
@@ -8,4 +8,5 @@ public class SceneConstants {
     public static final String MAINMENU = "Main Menu";
     public static final String NEWGAMEMENU = "New Game";
     public static final String CREDITMENU = "Credit";
+    public static final String LOADGAMEMENU = "Load";
 }
