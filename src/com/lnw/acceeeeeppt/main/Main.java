7cc50f18ca4537c72854294ac788c12978c93bfa
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-28 13:13:17 +0700

    feat: register load game view to main panel

--- src/com/lnw/acceeeeeppt/main/Main.java
+++ src/com/lnw/acceeeeeppt/main/Main.java
@@ -5,6 +5,7 @@ import javax.swing.SwingUtilities;
 import com.lnw.acceeeeeppt.scene.MainView;
 import com.lnw.acceeeeeppt.scene.menu.CreditController;
 import com.lnw.acceeeeeppt.scene.menu.CreditView;
+import com.lnw.acceeeeeppt.scene.menu.LoadGameView;
 import com.lnw.acceeeeeppt.scene.menu.MainMenuController;
 import com.lnw.acceeeeeppt.scene.menu.MainMenuView;
 import com.lnw.acceeeeeppt.scene.menu.NewGameController;
@@ -18,6 +19,7 @@ public class Main {
             MainMenuView mainMenuView = new MainMenuView();
             NewGameView newGameView = new NewGameView();
             CreditView creditView = new CreditView();
+            LoadGameView loadGameView = new LoadGameView();
 
             new MainMenuController(mainMenuView, mainView);
             new NewGameController(mainView, newGameView);
@@ -26,6 +28,7 @@ public class Main {
             mainView.registerPanel(mainMenuView, SceneConstants.MAINMENU);
             mainView.registerPanel(newGameView, SceneConstants.NEWGAMEMENU);
             mainView.registerPanel(creditView, SceneConstants.CREDITMENU);
+            mainView.registerPanel(loadGameView, SceneConstants.LOADGAMEMENU);
 
             mainView.switchPanelCard(SceneConstants.MAINMENU);
             mainView.setFrameVisibility(true);
