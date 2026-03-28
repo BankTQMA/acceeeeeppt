cb8c6924360f7b87c1a2ce1c14b03394a06084bb
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-28 13:10:47 +0700

    feat(ui): add back button

--- src/com/lnw/acceeeeeppt/scene/menu/LoadGameView.java
+++ src/com/lnw/acceeeeeppt/scene/menu/LoadGameView.java
@@ -1,18 +1,24 @@
 package com.lnw.acceeeeeppt.scene.menu;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Cursor;
 import java.awt.FlowLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import com.lnw.acceeeeeppt.ui.FontPresets;
 import com.lnw.acceeeeeppt.ui.MarginConstants;
 
 public class LoadGameView extends JPanel {
     private JPanel topBar;
     private JPanel contentPanel;
     private JPanel bottomBar;
+
+    private JButton backButton;
 
     public LoadGameView() {
         // Container initialisation
@@ -27,6 +33,15 @@ public class LoadGameView extends JPanel {
                 MarginConstants.GLOBALMARGIN, MarginConstants.GLOBALMARGIN));
         contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
         bottomBar.setLayout(new FlowLayout(FlowLayout.RIGHT));
+
+        backButton.setBorderPainted(false);
+        backButton.setContentAreaFilled(false);
+        backButton.setFocusPainted(false);
+        backButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        backButton.setFont(FontPresets.H2BOLDFONT);
+        backButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        topBar.add(backButton, BorderLayout.WEST);
 
         add(topBar, BorderLayout.NORTH);
         add(contentPanel, BorderLayout.CENTER);
