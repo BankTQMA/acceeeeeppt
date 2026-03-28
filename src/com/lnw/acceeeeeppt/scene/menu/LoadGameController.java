f9804ce3e3fa4f0db43958a0dd7f9fc420a83cc8
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-28 13:14:28 +0700

    feat(ui): add LoadGameController template class

---
+++ src/com/lnw/acceeeeeppt/scene/menu/LoadGameController.java
@@ -0,0 +1,13 @@
+package com.lnw.acceeeeeppt.scene.menu;
+
+import com.lnw.acceeeeeppt.scene.MainView;
+
+public class LoadGameController {
+    private LoadGameView loadGameView;
+    private MainView mainView;
+
+    public LoadGameController(LoadGameView loadGameView, MainView mainView) {
+        this.loadGameView = loadGameView;
+        this.mainView = mainView;
+    }
+}
