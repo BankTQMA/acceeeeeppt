56147a4043ef819f3cf3f6499bc56c32b79e064d
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-29 12:19:16 +0700

    feat(controller): add LevelFailController with class contructor and a link to action handler (unimplemented)

---
+++ src/com/lnw/acceeeeeppt/scene/transition/LevelFailController.java
@@ -0,0 +1,33 @@
+package com.lnw.acceeeeeppt.scene.transition;
+
+import javax.swing.JPanel;
+
+import com.lnw.acceeeeeppt.scene.MainView;
+import com.lnw.acceeeeeppt.ui.SceneConstants;
+
+public class LevelFailController {
+    LevelFailView levelFailView;
+    MainView mainView;
+    String currSceneIdentifier;
+    JPanel currentScenePanel;
+
+    public LevelFailController(LevelFailView levelFailView, MainView mainView, String currSceneIdentifier,
+            JPanel currScenePanel) {
+        this.levelFailView = levelFailView;
+        this.mainView = mainView;
+        this.currSceneIdentifier = currSceneIdentifier;
+        this.currentScenePanel = currScenePanel;
+
+        this.levelFailView.addRetryButtonActionListener(e -> onRetry());
+        this.levelFailView.addMainMenuButtonActionListener(e -> onMainMenu());
+    }
+
+    private void onRetry() {
+
+    }
+
+    private void onMainMenu() {
+
+    }
+
+}
