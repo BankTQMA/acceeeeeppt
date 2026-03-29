4fde098f1a9113107b026a43cf9e87a5268402c2
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-29 13:09:45 +0700

    refactor(controller): inverse if condition

--- src/com/lnw/acceeeeeppt/scene/menu/NewGameController.java
+++ src/com/lnw/acceeeeeppt/scene/menu/NewGameController.java
@@ -3,6 +3,7 @@ package com.lnw.acceeeeeppt.scene.menu;
 import com.lnw.acceeeeeppt.model.Difficulty;
 import com.lnw.acceeeeeppt.model.PlayerModel;
 import com.lnw.acceeeeeppt.scene.MainView;
+import com.lnw.acceeeeeppt.system.LevelLoader;
 import com.lnw.acceeeeeppt.system.SaveManager;
 import com.lnw.acceeeeeppt.ui.SceneConstants;
 
@@ -38,21 +39,20 @@ public class NewGameController {
     }
 
     public void onAccept() {
-        System.out.println("Acceeeeeppt Button Clicked! (Placeholder Action)");
-        if (newGameView.getDifficulty() == Difficulty.HARDCORE)
-            return;
-        PlayerModel playerModel = SaveManager.createNewPlayerModel(newGameView.getSaveNameText());
-        int status = SaveManager.saveToDisk(playerModel);
-        if (status == 2) {
-            newGameView.showJOptionPaneWarning("Error writing to disk.", "IO Exception");
-            return;
-        }
-        if (status == 1) {
-            newGameView.showJOptionPaneWarning("Filename already exists, please rename.", "Duplicate File");
-            return;
-        }
+        if (newGameView.getDifficulty() != Difficulty.HARDCORE) {
+            PlayerModel playerModel = SaveManager.createNewPlayerModel(newGameView.getSaveNameText());
+            int status = SaveManager.saveToDisk(playerModel);
+            if (status == 2) {
+                newGameView.showJOptionPaneWarning("Error writing to disk.", "IO Exception");
+                return;
+            }
+            if (status == 1) {
+                newGameView.showJOptionPaneWarning("Filename already exists, please rename.", "Duplicate File");
+                return;
+            }
 
-        loadGameController.addSaveEntriesView(playerModel.getSaveName(), playerModel.getCurrLevel().toString(),
-                playerModel.getCreatedDateTimeInstant().toString());
+            loadGameController.addSaveEntriesView(playerModel.getSaveName(), playerModel.getCurrLevel().toString(),
+                    playerModel.getCreatedDateTimeInstant().toString());
+        }
     }
 }
