81925bb4c3f1164a55e83e6fca1541ce2d82372f
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-29 11:21:45 +0700

    feat(ui): add necessary components and some configs

--- src/com/lnw/acceeeeeppt/scene/transition/LevelPassView.java
+++ src/com/lnw/acceeeeeppt/scene/transition/LevelPassView.java
@@ -1,7 +1,28 @@
 package com.lnw.acceeeeeppt.scene.transition;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class LevelPassView extends JPanel {
+import com.lnw.acceeeeeppt.ui.MarginConstants;
 
+public class LevelPassView extends JPanel {
+    private JPanel topPanel;
+    private JPanel buttonRowPanel;
+    private JLabel passLabel;
+
+    public LevelPassView() {
+        topPanel = new JPanel();
+        buttonRowPanel = new JPanel();
+        passLabel = new JLabel("PASSED");
+
+        // Container configuration
+        setLayout(new BorderLayout());
+        setBorder(BorderFactory.createEmptyBorder(MarginConstants.GLOBALMARGIN, MarginConstants.GLOBALMARGIN,
+                MarginConstants.GLOBALMARGIN, MarginConstants.GLOBALMARGIN));
+        buttonRowPanel.setLayout(new FlowLayout());
+    }
 }
