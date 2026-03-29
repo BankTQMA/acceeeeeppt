b5a3b84d1b588dd95527cc8f46ff2e1173cfb305
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-29 11:55:35 +0700

    feat(controller): add LevelPassController template class

---
+++ src/com/lnw/acceeeeeppt/scene/transition/LevelPassController.java
@@ -0,0 +1,15 @@
+package com.lnw.acceeeeeppt.scene.transition;
+
+public class LevelPassController {
+    public LevelPassController() {
+
+    }
+
+    private onMainMenu() {
+
+    }
+
+    private onContinue() {
+
+    }
+}
