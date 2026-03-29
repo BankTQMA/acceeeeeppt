332afa3c9b42d42b05c658a6171a7fc1a65c7403
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-29 13:07:11 +0700

    feat(system): update scene constants

--- src/com/lnw/acceeeeeppt/ui/SceneConstants.java
+++ src/com/lnw/acceeeeeppt/ui/SceneConstants.java
@@ -10,4 +10,6 @@ public class SceneConstants {
     public static final String LOADGAMEMENU = "Load";
     public static final String OPTIONMENU = "OPTIONMENU";
     public static final String CREDITMENU = "Credit";
+
+    public static final String LEVEL1 = "Level 1 - Term of Investment";
 }
