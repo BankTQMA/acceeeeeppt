c23d1efa75354984f0ca310da82d8141357f0e5a
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-29 10:23:55 +0700

    feat: modify level 9 view to allow main view to register the panel

--- src/com/lnw/acceeeeeppt/scene/level/level9_idle_game/Level9IdleMainView.java
+++ src/com/lnw/acceeeeeppt/scene/level/level9_idle_game/Level9IdleMainView.java
@@ -3,18 +3,11 @@ package com.lnw.acceeeeeppt.scene.level.level9_idle_game;
 import javax.swing.*;
 import java.awt.*;
 
-public class Level9IdleMainView {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Level 9 Idle Game");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(850, 750);
-        frame.setLayout(new GridLayout(2, 1));
+public class Level9IdleMainView extends JPanel {
+    public Level9IdleMainView(Level9IdleModel level9IdleModel) {
+        setLayout(new GridLayout(2, 1));
 
-        Level9IdleModel game = new Level9IdleModel();
-        frame.add(new Level9IdleTosView(game));
-        frame.add(new Level9IdleGameView(game));
-
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        add(new Level9IdleTosView(level9IdleModel));
+        add(new Level9IdleGameView(level9IdleModel));
     }
 }
