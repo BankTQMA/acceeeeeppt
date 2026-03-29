795c66d86d964036914b745cfe46d1003d8eab4b
Therdsak Chen <minecraftforce1234@gmail.com>
2026-03-29 11:34:11 +0700

    feat(ui): add main menu and save & continue button component and expose action listener setters

--- src/com/lnw/acceeeeeppt/scene/transition/LevelPassView.java
+++ src/com/lnw/acceeeeeppt/scene/transition/LevelPassView.java
@@ -3,8 +3,10 @@ package com.lnw.acceeeeeppt.scene.transition;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
@@ -16,11 +18,15 @@ public class LevelPassView extends JPanel {
     private JPanel topPanel;
     private JPanel buttonRowPanel;
     private JLabel passLabel;
+    private JButton mainMenuButton;
+    private JButton saveAndContinueButton;
 
     public LevelPassView() {
         topPanel = new JPanel();
         buttonRowPanel = new JPanel();
         passLabel = new JLabel("PASSED");
+        mainMenuButton = new JButton("Go Back to Main Menu");
+        saveAndContinueButton = new JButton("Save & Continue");
 
         setBackground(new Color(0x2CB714));
 
@@ -34,5 +40,13 @@ public class LevelPassView extends JPanel {
         passLabel.setFont(FontPresets.PASSFAILFONT);
         passLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(passLabel, BorderLayout.CENTER);
+    }
+
+    public void addMainMenuButtonActionListener(ActionListener l) {
+        mainMenuButton.addActionListener(l);
+    }
+
+    public void addSaveAndContinueButtonActionListener(ActionListener l) {
+        saveAndContinueButton.addActionListener(l);
     }
 }
